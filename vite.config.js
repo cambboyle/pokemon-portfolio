f4a68372b09a2d7eb16ef11c66dd44cc8dceaa6a
Cameron <cambboyle@gmail.com>
2024-12-15 16:56:55 +0000

    Feat: Add responsive design

--- vite.config.js
+++ vite.config.js
@@ -24,5 +24,10 @@ export default defineConfig({
   },
   server: {
     historyApiFallback: true
+  },
+  define: {
+    'process.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(process.env.VITE_EMAILJS_PUBLIC_KEY),
+    'process.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(process.env.VITE_EMAILJS_SERVICE_ID),
+    'process.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(process.env.VITE_EMAILJS_TEMPLATE_ID)
   }
 })
